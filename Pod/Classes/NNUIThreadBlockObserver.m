cbefa52cfcc16a295aafc4c7e9d41dc5e529f3b8
Koichi Yamamoto <noughts@gmail.com>
2015-11-25 01:03:12 +0900

    a

--- Pod/Classes/NNUIThreadBlockObserver.m
+++ Pod/Classes/NNUIThreadBlockObserver.m
@@ -32,22 +32,12 @@
     if( _lastTimeStamp ){
         CFTimeInterval gap = link.timestamp - _lastTimeStamp;
         if( gap > _threathold ){
-            [self logWithGap:gap];
+            NSLog( @"NNUIThreadBlockObserver: %@ms UIスレッドがブロックされました", @(ceil(gap*1000)) );
         }
     }
     _lastTimeStamp = link.timestamp;
 }
 
-
--(void)logWithGap:(CFTimeInterval)gap{
-    NSNumber* number = [[NSNumber alloc] initWithDouble:gap];
-    NSNumberFormatter* formatter = [[NSNumberFormatter alloc] init];
-    formatter.numberStyle = NSNumberFormatterDecimalStyle;
-    formatter.minimumFractionDigits = 3;
-    formatter.maximumFractionDigits = 3;
-    NSString* string = [formatter stringFromNumber:number];
-    NSLog( @"NNUIThreadBlockObserver: %@秒間 UIスレッドがブロックされました", string );
-}
 
 
 @end
